606e05466a0805871c60a9e0010c7290187a78d7
Bernd Porr <mail@berndporr.me.uk>
2019-07-27 20:00:35 +0100

    Changed name so that it's the same

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(
-    name='ecgdetectors',
+    name='py-ecg-detectors',
     version='1.0.0',
     description="Seven ECG heartbeat detection algorithms and heartrate variability analysis",
     long_description=long_description,
