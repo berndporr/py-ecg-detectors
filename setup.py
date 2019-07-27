865bf7626555a290132d3327d4104d55bcb4a383
Bernd Porr <mail@berndporr.me.uk>
2019-07-27 21:39:36 +0100

    The wavelet package also needs to be present.

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ with open('README.rst') as f:
 
 setup(
     name='py-ecg-detectors',
-    version='0.9.1',
+    version='0.9.2',
     description="Seven ECG heartbeat detection algorithms and timedomain heartrate variability analysis",
     long_description=long_description,
     author='Luis Howell, Bernd Porr',
     author_email='luisbhowell@gmail.com, bernd.porr@glasgow.ac.uk',
     py_modules=['ecgdetectors','hrv'],
     include_package_data=True,
-    install_requires=['numpy','pywt','pathlib','scipy','biosppy','gatspy'],
+    install_requires=['numpy','pywt','pathlib','scipy','biosppy','gatspy','pywavelets'],
     zip_safe=False,
     url='https://github.com/berndporr/py-ecg-qrs-detectors',
     license='GPL 3.0',
+    data_files=[('templates',['templates/template_250hz.csv','templates/template_360hz.csv'])],
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
