4a1bf53e7369fd99c1491edffc77802c6a722ebe
Bernd Porr <mail@berndporr.me.uk>
2019-07-27 19:47:53 +0100

    Changed link & e-mail

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open('README.rst') as f:
 
 setup(
     name='ecgdetectors',
-    version='0.9.0',
+    version='1.0.0',
     description="Seven ECG heartbeat detection algorithms and heartrate variability analysis",
     long_description=long_description,
     author='Luis Howell and Bernd Porr',
-    author_email='luisbhowell@gmail.com',
+    author_email='bernd.porr@glasgow.ac.uk',
     py_modules=['ecgdetectors','hrv'],
     include_package_data=True,
     install_requires=['numpy','pywt','pathlib','scipy','biosppy','gatspy'],
     zip_safe=False,
-    url='https://github.com/luishowell/ecg-detectors',
+    url='https://github.com/berndporr/py-ecg-qrs-detectors',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GPL',
