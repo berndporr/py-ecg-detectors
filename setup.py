237aa536e5c0f3a3cd6e33889919391a5d09e602
Bernd Porr <mail@berndporr.me.uk>
2019-07-31 22:46:56 +0100

    New version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='py-ecg-detectors',
-    version='0.9.5',
+    version='0.9.6',
     description="Seven ECG heartbeat detection algorithms and heartrate variability analysis",
     long_description=long_description,
     author='Luis Howell, Bernd Porr',
