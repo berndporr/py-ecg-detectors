f81ac38361d6b2b4121ec8e62d9a2e56bff77bc3
Clemens Brunner <clemens.brunner@gmail.com>
2019-11-18 13:14:27 +0100

    PEP8

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 from setuptools import setup
 
 with open('README.rst') as f:
@@ -8,18 +6,20 @@ with open('README.rst') as f:
 setup(
     name='py-ecg-detectors',
     version='0.9.6',
-    description="Seven ECG heartbeat detection algorithms and heartrate variability analysis",
+    description=("Seven ECG heartbeat detection algorithms and heartrate "
+                 "variability analysis"),
     long_description=long_description,
     author='Luis Howell, Bernd Porr',
     author_email='luisbhowell@gmail.com, bernd.porr@glasgow.ac.uk',
-    py_modules=['ecgdetectors','hrv'],
+    py_modules=['ecgdetectors', 'hrv'],
     include_package_data=True,
     install_requires=['numpy', 'scipy', 'pathlib2;python_version<"3.4"',
                       'biosppy', 'gatspy', 'pywavelets'],
     zip_safe=False,
     url='https://github.com/berndporr/py-ecg-qrs-detectors',
     license='GPL 3.0',
-    data_files=[('templates',['templates/template_250hz.csv','templates/template_360hz.csv'])],
+    data_files=[('templates', ['templates/template_250hz.csv',
+                               'templates/template_360hz.csv'])],
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
