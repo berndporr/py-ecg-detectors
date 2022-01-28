d3e785f7d85934ffe8ea13a6a7d8112d6e9930f3
Bernd Porr <mail@berndporr.me.uk>
2022-01-28 09:36:28 +0000

    Updated the doc

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -3,5 +3,5 @@ var searchData=
   ['sdann_18',['SDANN',['../classhrv_1_1HRV.html#a2f06b90967d0aaf25aed8e3acf1a0963',1,'hrv::HRV']]],
   ['sdnn_19',['SDNN',['../classhrv_1_1HRV.html#a89ad722309b569b4531b1491d61cfc32',1,'hrv::HRV']]],
   ['sdsd_20',['SDSD',['../classhrv_1_1HRV.html#a3b7bfd38942d77d2e7989102c1971eee',1,'hrv::HRV']]],
-  ['swt_5fdetector_21',['swt_detector',['../classecgdetectors_1_1Detectors.html#a9cd3e2ac068417bfec07301807e99d6b',1,'ecgdetectors::Detectors']]]
+  ['swt_5fdetector_21',['swt_detector',['../classecgdetectors_1_1Detectors.html#ad0dc700552e3dda4b9795a5ee0e24c37',1,'ecgdetectors::Detectors']]]
 ];
